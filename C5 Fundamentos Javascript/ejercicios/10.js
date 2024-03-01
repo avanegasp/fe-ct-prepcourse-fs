3b6c017894ba951bd763bdcddc22b3c9da0592b0
avanegas <arco527@gmail.com>
2024-03-01 17:12:20 -0500

    Add moduloJS files and answering JS exercises

--- C5 Fundamentos Javascript/ejercicios/10.js
+++ C5 Fundamentos Javascript/ejercicios/10.js
@@ -3,6 +3,13 @@ function esFechaValida(fecha) {
   // Comprueba si este corresponde a una fecha válida.
   // Si es así, retorna true, sino retorna false.
   // Tu código:
+  if(Date.parse(fecha)){
+    return true;
+ }else{
+    return false;
+    }
 }
+
+console.log(esFechaValida(2008-13-10))
 
 module.exports = esFechaValida;
