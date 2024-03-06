d4e615f7f7163d7bebcb6ba34086b39524f82c1b
avanegas <arco527@gmail.com>
2024-03-06 17:49:46 -0500

    Excercises 19 done

--- C6 Bucles/ejercicios/19.js
+++ C6 Bucles/ejercicios/19.js
@@ -2,6 +2,15 @@ function sumarHastaN(n) {
   // La función recibe un número "n" por argumento.
   // Devuelve la suma de todos los números desde 1 hasta n.
   // Tu código:
+
+  let todosNumeros = 0 
+  
+  for (let i=1; i <= n; i++ ){
+    todosNumeros += i
+  }
+  return todosNumeros
 }
+
+sumarHastaN(5)
 
 module.exports = sumarHastaN;
