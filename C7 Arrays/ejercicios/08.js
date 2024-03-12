d610b3650803812c42b796309f134a98d4156927
avanegas <arco527@gmail.com>
2024-03-12 09:49:17 -0500

    Resolving code

--- C7 Arrays/ejercicios/08.js
+++ C7 Arrays/ejercicios/08.js
@@ -3,6 +3,14 @@ function encontrarElemento(num, array) {
    // Si lo encuentras debes retornar el INDICE en el que se encuentra dentro del array.
    // Si no se encuentra, retorna -1.
    // Tu código:
+
+   for(let i=0; i < array.length; i++){
+      if (num === array[i]){
+      return array.indexOf(num)
+      }
+   }return -1
 }
+
+encontrarElemento(2,[1,2,3])
 
 module.exports = encontrarElemento;
