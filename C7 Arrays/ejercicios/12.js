828feaf63c7633d8b4f8301e0c1ed69bc2662a28
avanegas <arco527@gmail.com>
2024-04-08 20:44:05 -0500

    Next commit

--- C7 Arrays/ejercicios/12.js
+++ C7 Arrays/ejercicios/12.js
@@ -2,6 +2,16 @@ function convertirStringAMayusculas(array) {
   // Convierte a mayúsculas todos los strings del array.
   // Retorna el arreglo resultante.
   // Tu código:
+
+  if (array.length === 0) {
+    return [];
 }
+
+return array.map(function(elemento) {
+    return elemento.toUpperCase();
+});
+}
+
+convertirStringAMayusculas(["hola", "mundo", "soy", "Angie"])
 
 module.exports = convertirStringAMayusculas;
